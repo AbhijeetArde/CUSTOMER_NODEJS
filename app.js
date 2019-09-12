90ec31d78a4313348286a2cc8295ae3ff87adb8b
AbhijeetArde <41187312+AbhijeetArde@users.noreply.github.com>
2019-09-12 15:12:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,47 @@
-
 'use strict';
-
 const express = require('express');
 const crypto = require('crypto');
-
 const app = express();
 app.enable('trust proxy');
-
-const {Datastore} = require('@google-cloud/datastore');
-
-// Instantiate a datastore client
+const {
+    Datastore
+} = require('@google-cloud/datastore');
 const datastore = new Datastore();
-
-
-const insertCust = visit => {
-  return datastore.save({
-    key: datastore.key('Customername'),
-    data: visit,
-  });
-};
-
-
-const getVisits = (id) => {
-	id = id || ""
-	const query = datastore.createQuery('Customername');
-	const selectq = query.order('Id', {descending: true}).limit(10);
-	if (id != ""){
-		query.filter('Id', '=', id)
-		}
-  return datastore.runQuery(query);
+const getCustomers = (id) => {
+    id = id || ""
+    const query = datastore.createQuery('Customername');
+    const selectq = query.order('Id', {
+        descending: true
+    }).limit(10);
+    if (id != "") {
+        query.filter('Id', '=', id)
+    }
+    return datastore.runQuery(query);
 };
 
 app.get('/getCustomers', async (req, res, next) => {
-  
-  try {
-    //await insertCust(cust);
-	const id =req.query.id
-    const [customer_data] = await getVisits(id);
-    //const customers = customer_data.map(entity => customer_data[entity.customername] = customer_data[entity.Id]);
-    res
-      .status(200)
-      .set('Content-Type', 'application/json')
-      .json({customer_data})
-      .end();
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const [customer_data] = await getCustomers("");
+        res.json(customer_data)
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.get('/getCustomer', async (req, res, next) => {
+    try {
+        const id = req.query.id
+        const [customer_data] = await getCustomers(id);
+        res.json(customer_data[0])
+    } catch (error) {
+        next(error);
+    }
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
 });
 
 module.exports = app;
